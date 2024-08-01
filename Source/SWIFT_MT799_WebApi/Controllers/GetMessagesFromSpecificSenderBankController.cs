80dc90f38be2b638543f924d9aa09be6fb2d2508
Daniel Damianov <56774476+danieldamianov@users.noreply.github.com>
2024-08-02 01:15:37 +0300

    Added second functionality of the WebApi (still with sample data).

---
+++ Source/SWIFT_MT799_WebApi/Controllers/GetMessagesFromSpecificSenderBankController.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationLogic.Models;
+using ApplicationLogic.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SWIFT_MT799_WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/get-messages-from-sender")]
+    public class GetMessagesFromSpecificSenderBankController : ControllerBase
+    {
+
+        private readonly ILogger<GetMessagesFromSpecificSenderBankController> _logger;
+        private readonly IMediator mediator;
+
+        public GetMessagesFromSpecificSenderBankController
+            (ILogger<GetMessagesFromSpecificSenderBankController> logger, IMediator mediator)
+        {
+            this._logger = logger;
+            this.mediator = mediator;
+        }
+
+        // TODO:: Return IActionResult
+        [HttpGet("{bankCode}", Name = "GetMessagesFromSpecificSenderBank")]
+        public async Task<ICollection<SWIFT_MT799_Message_Model>> GetMessages(string bankCode)
+        {
+            ICollection<SWIFT_MT799_Message_Model> response = 
+                await this.mediator.Send(new GetMessagesFromSpecificSenderBankQuery(bankCode));
+
+            // TODO:: RETURN SOME IACTION RESULT
+            return response;
+        }
+    }
+}
