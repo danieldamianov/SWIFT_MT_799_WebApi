bac5f4de69d08d16c814a2f443572d9e5e3e5a1f
Daniel Damianov <56774476+danieldamianov@users.noreply.github.com>
2024-08-03 17:29:56 +0300

    Added tests project.

---
+++ Source/SWIFT_MT_799_Application_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SWIFT_MT_799_Application_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
